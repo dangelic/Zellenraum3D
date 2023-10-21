aa848b04ef99b89a61b28d1302d227b00d6d8c8f
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-10-21 19:28:10 +0200

    feat(init): setup initial electron structure

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+let mainWindow: BrowserWindow | null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1150,
+    height: 950,
+    webPreferences: {
+      nodeIntegration: true, // Allow Node.js integration in the renderer process
+      contextIsolation: false, // Disable context isolation for simpler integration
+      preload: path.join(__dirname, 'preload.js')
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, '..', 'index.html'));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
