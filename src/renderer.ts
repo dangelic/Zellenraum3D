a4ac6513f6295166d64e1618e4137ab4c5b8379f
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-10-21 20:30:17 +0200

    feat(3D): create outer box and box hiding

--- src/renderer.ts
+++ src/renderer.ts
@@ -20,11 +20,35 @@ const gridSize = 10;
 const cubeSize = 1;
 const grid = new Array(gridSize);
 
-// Calculate the center of the grid
+// Calculate the total number of inner cubes
+const totalInnerCubes = gridSize * gridSize * gridSize;
+
+// Calculate the center of all inner cubes
 const centerX = (gridSize - 1) * cubeSize / 2;
 const centerY = (gridSize - 1) * cubeSize / 2;
 const centerZ = (gridSize - 1) * cubeSize / 2;
-scene.position.set(0, -centerY, 0); // Adjust the Y position to center the grid
+
+const innerCenterX = (centerX * 2) / gridSize;
+const innerCenterY = (centerY * 2) / gridSize;
+const innerCenterZ = (centerZ * 2) / gridSize;
+
+// Calculate the position of the outer box's center
+const outerCenterX = 0; // Set to 0 to ensure the pivot is at the center
+const outerCenterY = 0;
+const outerCenterZ = 0;
+
+// Create an outer container cube
+const outerGeometry = new THREE.BoxGeometry(cubeSize * gridSize, cubeSize * gridSize, cubeSize * gridSize);
+
+// Manually create the wireframe for the outer box
+const wireframe = new THREE.WireframeGeometry(outerGeometry);
+const outerMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00, linewidth: 2 }); // Adjust the linewidth as needed
+const outerCube = new THREE.LineSegments(wireframe, outerMaterial);
+outerCube.position.set(outerCenterX, outerCenterY, outerCenterZ); // Adjust the position based on the calculated center
+scene.add(outerCube);
+
+// Create a material for the inner cubes to make them black
+const blackMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff, wireframe: true });
 
 for (let x = 0; x < gridSize; x++) {
   grid[x] = new Array(gridSize);
@@ -35,10 +59,10 @@ for (let x = 0; x < gridSize; x++) {
     for (let z = 0; z < gridSize; z++) {
       // Create a cube for each grid cell
       const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
-      grid[x][y][z] = new THREE.Mesh(geometry, material);
-      grid[x][y][z].position.set(x * cubeSize - centerX, y * cubeSize, z * cubeSize - centerZ); // Adjust the positions based on the center
-      scene.add(grid[x][y][z]);
+
+      grid[x][y][z] = new THREE.Mesh(geometry, blackMaterial); // Use the black material for inner cubes
+      grid[x][y][z].position.set(x * cubeSize - centerX, y * cubeSize - centerY, z * cubeSize - centerZ + 0.02); // Add a small offset
+      outerCube.add(grid[x][y][z]); // Add inner cubes to the outer container
     }
   }
 }
