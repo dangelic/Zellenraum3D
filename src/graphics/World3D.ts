4262585fa9c6b665e4279b4536a2ac50474f6d70
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-11-01 01:00:14 +0100

    refactor(world): take mesh creation from experiment

--- src/graphics/World3D.ts
+++ src/graphics/World3D.ts
@@ -1,200 +1,106 @@
-import { scene } from '../renderer';
 import * as THREE from 'three';
-
-import { Seeds } from './Seeds';
+import { scene } from '../renderer';
 
 export class World3D {
-  // Singleton
-  private static instance: World3D | null = null;
-
-  private scene: THREE.Scene;
-  private isCreatingCells: boolean = false; // Flag to track cube creation
-
-  private cellSize: number;
   private worldSize: number;
+  private cellSize: number;
+  private cellOffset: number;
 
-  private cellGeometry: THREE.BoxGeometry;
-  private frameBoxSize: number;
-  private frameBoxGeometry: THREE.BoxGeometry;
-  private frameBoxEdgeGeometry: THREE.EdgesGeometry;
-  private frameBoxEdgeLines: THREE.LineSegments;
+  private currentGeneration: boolean[][][];
+  private generationCount: number;
 
-  private cellArray: boolean[][][]; // 3D array to store cell visibility
+  public constructor(worldSize: number, cellSize: number, cellOffset: number) {
+    this.worldSize = worldSize;
+    this.cellSize = cellSize;
+    this.cellOffset = cellOffset;
 
-  private constructor() {
-    this.scene = scene; // from renderer.ts
+    this.currentGeneration = this.createEmptyGeneration();
+    this.generationCount = 0;
 
-    this.cellSize = 1;
-    this.worldSize = 30;
+    this.createFramebox();
+  }
 
-    this.cellGeometry = new THREE.BoxGeometry(
+  // Getter & Setter
+  //
+  //
+
+  public getCurrentGeneration(): boolean[][][] {
+    return this.currentGeneration;
+  }
+
+  public setCurrentGeneration(currentGeneration: boolean[][][]): void {
+    this.generationCount++; // Adjust the counter
+    console.log('Generation Count: ' + this.generationCount);
+
+    this.currentGeneration = currentGeneration;
+    this.addCellsToScene();
+  }
+
+  private addCellsToScene = (): void => {
+    // Clear scene and Re-add frameBox
+    scene.clear();
+    this.createFramebox();
+
+    // Create new InstancedMesh and add it to the scene
+    const geometry = new THREE.BoxGeometry(
       this.cellSize,
       this.cellSize,
       this.cellSize,
     );
-    this.frameBoxSize = this.cellSize * this.worldSize + this.cellSize;
-    this.frameBoxGeometry = new THREE.BoxGeometry(
-      this.frameBoxSize,
-      this.frameBoxSize,
-      this.frameBoxSize,
-    );
-    this.frameBoxEdgeGeometry = new THREE.EdgesGeometry(this.frameBoxGeometry);
-    this.frameBoxEdgeLines = new THREE.LineSegments(
-      this.frameBoxEdgeGeometry,
-      new THREE.LineBasicMaterial({ color: 0x00ff00 })
-    );
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const numInstances = Math.pow(this.worldSize, 3); // cubic relation to worldSize (x*y*z cells)
+    const cellMesh = new THREE.InstancedMesh(geometry, material, numInstances);
+    scene.add(cellMesh);
 
-    this.frameBoxEdgeLines.position.set(0, 0, 0);
-    this.scene.add(this.frameBoxEdgeLines);
-
-    // Initialize the 3D array to store cell visibility
-    this.cellArray = new Array(this.worldSize);
-    for (let x = 0; x < this.worldSize; x++) {
-      this.cellArray[x] = new Array(this.worldSize);
-      for (let y = 0; y < this.worldSize; y++) {
-        this.cellArray[x][y] = new Array(this.worldSize).fill(false);
-      }
-    }
-    //this.cellArray = Seeds.getRandomSeed(this.cellArray, 0.05);
-    this.cellArray = Seeds.getClusteredSeed(this.cellArray, 50)
-  }
-
-  public static getInstance(): World3D {
-    if (!World3D.instance) {
-      World3D.instance = new World3D();
-    }
-    return World3D.instance;
-  }
-
-  public clearWorld(): void {
-    const children = this.scene.children;
-    for (let i = children.length - 1; i >= 0; i--) {
-      const child = children[i];
-      if (child !== this.frameBoxEdgeLines) {
-        // Don't remove the frame box lines
-        this.scene.remove(child);
-      }
-    }
-    this.isCreatingCells = false; // Set the flag to stop cube creation
-    this.clearCellArray();
-  }
-
-  public getWorldSize(): number {
-    return this.worldSize;
-  }
-
-  public getCellArray(): boolean[][][] {
-    return this.cellArray;
-  }
-
-  public startDemo(msDelay: number, addInstantly: boolean = true): void {
-    if (this.isCreatingCells) {
-      // If cube creation is in progress, don't start a new one
-      return;
-    }
-
-    this.isCreatingCells = true; // Set the flag to indicate cube creation is in progress;
-
-    if (addInstantly) {
-      this.addCubesInstantly();
-    } else {
-      this.addCubesIncrementally(msDelay, 0, 0, 0);
-    }
-  }
-
-  // Setter to set cell visibility
-  public setCellVisibility(x: number, y: number, z: number, visible: boolean): void {
-    this.cellArray[x][y][z] = visible;
-  }
-
-  private clearCellArray(): void {
     for (let x = 0; x < this.worldSize; x++) {
       for (let y = 0; y < this.worldSize; y++) {
         for (let z = 0; z < this.worldSize; z++) {
-          this.cellArray[x][y][z] = false;
-        }
-      }
-    }
-  }
+          let isCellVisible = this.currentGeneration[x][y][z];
 
-  public addCubesInstantly(cellArray?) {
-    for (let x = 0; x < this.worldSize; x++) {
-      for (let y = 0; y < this.worldSize; y++) {
-        for (let z = 0; z < this.worldSize; z++) {
-          let isCellVisible: boolean;
-          if (arguments.length === 1) isCellVisible = cellArray[x][y][z];
-          else isCellVisible = this.cellArray[x][y][z];
-
+          const matrix = new THREE.Matrix4();
           if (isCellVisible) {
-            // Calculate the distance from the center of the grid
-            const distance = Math.sqrt(
-              (x - this.worldSize / 2) ** 2 +
-              (y - this.worldSize / 2) ** 2 +
-              (z - this.worldSize / 2) ** 2
+            // Adjust cell positions based on worldSize
+            matrix.setPosition(
+              (x - this.worldSize / 2) * (this.cellSize + this.cellOffset),
+              (y - this.worldSize / 2) * (this.cellSize + this.cellOffset),
+              (z - this.worldSize / 2) * (this.cellSize + this.cellOffset),
             );
-
-            // Calculate a color based on the distance (yellow to red gradient)
-            const hue = 0.1667 + (distance / (this.worldSize * 1.5)) * 0.8333; // From yellow (0.1667) to red (1.0)
-            const saturation = 1; // Full saturation
-            const lightness = 1 - (distance / (this.worldSize * 1.5)); // Adjust the lightness based on distance (from yellow to red)
-
-            // Create the cell with the calculated color and add it to the scene
-            const color = new THREE.Color();
-            color.setHSL(hue, saturation, lightness);
-            const cellMaterial = new THREE.MeshBasicMaterial({ color });
-            const cell = new THREE.Mesh(this.cellGeometry, cellMaterial);
-            cell.position.set(
-              x * this.cellSize - this.worldSize / 2,
-              y * this.cellSize - this.worldSize / 2,
-              z * this.cellSize - this.worldSize / 2
+            cellMesh.setMatrixAt(
+              x * this.worldSize * this.worldSize + y * this.worldSize + z,
+              matrix,
             );
-            this.scene.add(cell);
           }
         }
       }
     }
+  };
 
-    this.isCreatingCells = false; // Cube creation is complete
-}
-
-
-
-  private addCubesIncrementally(msDelay, x, y, z) {
-    if (!this.isCreatingCells) {
-      // If cube creation should be stopped, exit the function
-      return;
+  private createEmptyGeneration(): boolean[][][] {
+    const currentGeneration = new Array(this.worldSize);
+    for (let x = 0; x < this.worldSize; x++) {
+      currentGeneration[x] = new Array(this.worldSize);
+      for (let y = 0; y < this.worldSize; y++) {
+        currentGeneration[x][y] = new Array(this.worldSize).fill(false);
+      }
     }
+    return currentGeneration;
+  }
 
-    // Determine if the cell should be visible
-    const isCellVisible = this.cellArray[x][y][z];
+  private createFramebox(): void {
+    const frameBoxSize =
+      this.worldSize * (this.cellSize + this.cellOffset) - this.cellSize;
+    const frameBoxGeometry = new THREE.BoxGeometry(
+      frameBoxSize,
+      frameBoxSize,
+      frameBoxSize,
+    );
+    const frameBoxEdgeGeometry = new THREE.EdgesGeometry(frameBoxGeometry);
+    const frameBox = new THREE.LineSegments(
+      frameBoxEdgeGeometry,
+      new THREE.LineBasicMaterial({ color: 0x00ffff }),
+    );
 
-    if (isCellVisible) {
-      // Create the cell and add it to the scene
-      const cell = new THREE.Mesh(this.cellGeometry, new THREE.MeshBasicMaterial({ color: 0x0000ff }));
-      cell.position.set(
-        x * this.cellSize - this.worldSize / 2,
-        y * this.cellSize - this.worldSize / 2,
-        z * this.cellSize - this.worldSize / 2
-      );
-      this.scene.add(cell);
-    }
-
-    // Continue with the next cell
-    if (x < this.worldSize - 1) {
-      setTimeout(() => {
-        this.addCubesIncrementally(msDelay, x + 1, y, z);
-      }, msDelay);
-    } else if (y < this.worldSize - 1) {
-      setTimeout(() => {
-        this.addCubesIncrementally(msDelay, 0, y + 1, z);
-      }, msDelay);
-    } else if (z < this.worldSize - 1) {
-      setTimeout(() => {
-        this.addCubesIncrementally(msDelay, 0, 0, z + 1);
-      }, msDelay);
-    } else {
-      // Cube creation is complete
-      this.isCreatingCells = false;
-    }
+    frameBox.position.set(0, 0, 0);
+    scene.add(frameBox);
   }
 }
