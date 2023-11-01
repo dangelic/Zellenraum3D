04ce9dfb2a2f2d49aa739a6a72fd15dbb9784adf
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-11-01 01:00:31 +0100

    feat(rules): implement cloud rule

--- src/rules/Rules.ts
+++ src/rules/Rules.ts
@@ -1,89 +1,59 @@
-import { Neighborhood } from "./Neighborhood";
+import { Neighborhood } from './Neighborhood';
 
 export class Rules {
-
-
-  static customRule(
-    world: boolean[][][],
-    x: number,
-    y: number,
-    z: number,
-  ): boolean {
-    const neighbors = Neighborhood.getMooreNeighborhood(x, y, z);
-
-    let aliveNeighbors = 0;
-
-    for (const [nx, ny, nz] of neighbors) {
-      if (
-        nx >= 0 &&
-        nx < world.length &&
-        ny >= 0 &&
-        ny < world[0].length &&
-        nz >= 0 &&
-        nz < world[0][0].length
-      ) {
-        if (world[nx][ny][nz]) {
-          aliveNeighbors++;
+  static applyClouds(currentGeneration: boolean[][][]): boolean[][][] {
+    const worldSize = currentGeneration.length;
+  
+    // Create a new world array to store the next state
+    const nextGeneration = new Array(worldSize);
+    for (let x = 0; x < worldSize; x++) {
+      nextGeneration[x] = new Array(worldSize);
+      for (let y = 0; y < worldSize; y++) {
+        nextGeneration[x][y] = new Array(worldSize).fill(false);
+      }
+    }
+  
+    for (let x = 0; x < worldSize; x++) {
+      for (let y = 0; y < worldSize; y++) {
+        for (let z = 0; z < worldSize; z++) {
+          const neighbors = Neighborhood.getMooreNeighborhood(x, y, z);
+          let aliveNeighbors = 0;
+  
+          for (const [nx, ny, nz] of neighbors) {
+            if (
+              nx >= 0 &&
+              nx < worldSize &&
+              ny >= 0 &&
+              ny < worldSize &&
+              nz >= 0 &&
+              nz < worldSize
+            ) {
+              if (currentGeneration[nx][ny][nz]) {
+                aliveNeighbors++;
+              }
+            }
+          }
+  
+          // Apply rules based on the count of alive neighbors
+          if (currentGeneration[x][y][z]) {
+            if (
+              aliveNeighbors >= 13 &&
+              aliveNeighbors <= 26
+            ) {
+              nextGeneration[x][y][z] = true; // Cell survives with the specified range of neighbors
+            }
+          } else {
+            if (
+              (aliveNeighbors >= 13 && aliveNeighbors <= 14) ||
+              (aliveNeighbors >= 17 && aliveNeighbors <= 19)
+            ) {
+              nextGeneration[x][y][z] = true; // Cell is born in an empty location with the specified range of neighbors
+            }
+          }
         }
       }
     }
-
-    // Apply custom rules based on the count of alive neighbors
-    if (world[x][y][z]) {
-      // Any live cell with 5-6 live neighbors survives.
-      if (aliveNeighbors >= 5 && aliveNeighbors <= 6) {
-        return true; // Cell survives
-      } else {
-        return false; // Cell dies
-      }
-    } else {
-      // Any dead cell with 4 live neighbors becomes a live cell.
-      if (aliveNeighbors === 4) {
-        return true; // Cell is born
-      } else {
-        return false; // Cell stays dead
-      }
-    }
-  }
-
-  static apply445M(
-    world: boolean[][][],
-    x: number,
-    y: number,
-    z: number,
-  ): string {
-    const neighbors = Neighborhood.getMooreNeighborhood(x, y, z);
-
-    let aliveNeighbors = 0;
-
-    for (const [nx, ny, nz] of neighbors) {
-      if (
-        nx >= 0 &&
-        nx < world.length &&
-        ny >= 0 &&
-        ny < world[0].length &&
-        nz >= 0 &&
-        nz < world[0][0].length
-      ) {
-        if (world[nx][ny][nz]) {
-          aliveNeighbors++;
-        }
-      }
-    }
-
-    // Apply rules based on the count of alive neighbors
-    if (world[x][y][z]) {
-      if (aliveNeighbors === 4) {
-        return 'survives'; // Cell survives with 4 neighbors
-      } else {
-        return 'died'; // Cell dies with any other number of neighbors
-      }
-    } else {
-      if (aliveNeighbors === 4) {
-        return 'born'; // Cell is born in an empty location with 4 neighbors
-      }
-    }
-
-    return 'empty'; // No cell
-  }
+  
+    return nextGeneration;
+  }  
 }
