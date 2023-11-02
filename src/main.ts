e74696c7f7fd74854254a0a7f2b05ea7589d3069
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-11-03 00:33:30 +0100

    chore(comments)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { app, BrowserWindow } from "electron";
 import * as path from "path";
 
+
+/**
+ * ** The main window of the Electron application "Zellenraum3D".
+ * 
+ * Zellenraum3D is a captivating 3D visualization of cellular automata, powered by the THREE.js library.
+ * It provides a dynamic and interactive representation of evolving cellular structures.
+ * 
+ * From here on: Go to renderer.ts where the call-chain starts.
+ */
+
+/**
+ 
+ *
+ * @type {BrowserWindow | null}
+ */
 let mainWindow: BrowserWindow | null;
 
+/**
+ * Create the main application window.
+ */
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1300,
@@ -14,22 +32,29 @@ function createWindow() {
         },
     });
 
+    // Load the HTML file into the main window
     mainWindow.loadFile(path.join(__dirname, "..", "index.html"));
+
+    // Set the main window to fullscreen mode
     mainWindow.setFullScreen(true);
 
+    // Handle the "closed" event of the main window
     mainWindow.on("closed", () => {
         mainWindow = null;
     });
 }
 
+// Create the main window when the Electron app is ready
 app.on("ready", createWindow);
 
+// Quit the app when all windows are closed (except on macOS)
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
         app.quit();
     }
 });
 
+// Create a new window when the app is activated (on macOS)
 app.on("activate", () => {
     if (mainWindow === null) {
         createWindow();
--- src/preload.ts
+++ src/preload.ts
@@ -1,9 +1,27 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
+/**
+ * Expose IPC (Inter-Process Communication) functions to the renderer process.
+ *
+ * @namespace electron
+ */
 contextBridge.exposeInMainWorld("electron", {
+    /**
+     * Send a message to the main process.
+     *
+     * @param {string} channel - The channel to send the message on.
+     * @param {any} data - The data to send with the message.
+     */
     send: (channel, data) => {
         ipcRenderer.send(channel, data);
     },
+
+    /**
+     * Receive a message from the main process.
+     *
+     * @param {string} channel - The channel to listen for messages on.
+     * @param {Function} func - The callback function to handle received messages.
+     */
     receive: (channel, func) => {
         ipcRenderer.on(channel, (event, ...args) => func(...args));
     },
